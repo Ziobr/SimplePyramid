8a0665a47f0c47144ccb2fe34f018009a40ab22c
nuvemdeoort <server.deer@gmail.com>
2015-03-25 13:30:51 +0300

    Changed size and position

--- SimplePyramid/SimplePyramid/MainWindow.xaml.cs
+++ SimplePyramid/SimplePyramid/MainWindow.xaml.cs
@@ -67,12 +67,12 @@ namespace SimplePyramid
         {
             Model3DGroup triangle = new Model3DGroup();
             Point3D p0 = new Point3D(0, 0, 0);
-            Point3D p1 = new Point3D(5, 0, 0);
+            Point3D p1 = new Point3D(5, -5, 0);
             Point3D p2 = new Point3D(5, 0, 5);
             Point3D p3 = new Point3D(0, 0, 5);
-            Point3D p4 = new Point3D(0, 5, 0);
-            Point3D p5 = new Point3D(5, 5, 0);
-            Point3D p6 = new Point3D(5, 5, 5);
+            Point3D p4 = new Point3D(0, 0, 0);
+            Point3D p5 = new Point3D(5, 0, 0);
+            Point3D p6 = new Point3D(5, 0, 5);
 
 
             //            triangle.Children.Add(CreateTriangleModel(p1, p4, p3));
