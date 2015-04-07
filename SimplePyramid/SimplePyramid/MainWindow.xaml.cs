016a115dde414a13e05e2a7a3678d3514bfc66eb
Ziobr <equus.quagga@gmail.com>
2015-04-08 00:49:46 +0300

    added checkbox

--- SimplePyramid/SimplePyramid/MainWindow.xaml.cs
+++ SimplePyramid/SimplePyramid/MainWindow.xaml.cs
@@ -21,24 +21,31 @@ namespace SimplePyramid
 
         private int Length = 10;
 
+
         private Model3DGroup CreateTriangleModel(Point3D p0, Point3D p1, Point3D p2)
         {
             var myMeshBuilder = new MeshBuilder();
-            var edgeMeshBuilder = new MeshBuilder();
             myMeshBuilder.AddTriangle(p0, p1, p2);
+            var mesh = myMeshBuilder.ToMesh(true);
+            var material = MaterialHelper.CreateMaterial(this.myColor);
+            Model3DGroup Group = new Model3DGroup();
+            Group.Children.Add(new GeometryModel3D { Geometry = mesh, Material = material });
+            return Group;
+        }
+
+        private Model3DGroup CreateTriangleEdgesModel(Point3D p0, Point3D p1, Point3D p2)
+        {
+            var edgeMeshBuilder = new MeshBuilder();
             edgeMeshBuilder.AddCylinder(p0, p1, 0.3, 100);
             edgeMeshBuilder.AddCylinder(p1, p2, 0.3, 100);
             edgeMeshBuilder.AddCylinder(p2, p0, 0.3, 100);
-            var mesh = myMeshBuilder.ToMesh(true);
             var edgeMesh = edgeMeshBuilder.ToMesh(true);
-            var material = MaterialHelper.CreateMaterial(this.myColor);
             var edgeMaterial = MaterialHelper.CreateMaterial(this.myColor);
-
             Model3DGroup Group = new Model3DGroup();
-            //Group.Children.Add(new GeometryModel3D { Geometry = mesh, Material = material });
             Group.Children.Add(new GeometryModel3D { Geometry = edgeMesh, Material = edgeMaterial });
             return Group;
         }
+
         private Vector3D CalculateTraingleNormal(Point3D p0, Point3D p1, Point3D p2)
         {
             Vector3D v0 = new Vector3D(
@@ -56,6 +63,7 @@ namespace SimplePyramid
             }
             else this.Length = 10;
             Model3DGroup triangle = new Model3DGroup();
+            Model3DGroup triangleEdges = new Model3DGroup();
 
             Point3D p0 = new Point3D(0, 0, 0);
             Point3D p1 = new Point3D(this.Length, -this.Length, 0);
@@ -70,9 +78,28 @@ namespace SimplePyramid
             triangle.Children.Add(CreateTriangleModel(p5, p4, p6));
             triangle.Children.Add(CreateTriangleModel(p6, p4, p1));
 
+            triangleEdges.Children.Add(CreateTriangleModel(p1, p5, p6));
+            triangleEdges.Children.Add(CreateTriangleModel(p1, p4, p5));
+            triangleEdges.Children.Add(CreateTriangleModel(p5, p4, p6));
+            triangleEdges.Children.Add(CreateTriangleModel(p6, p4, p1));
 
             this.MainViewPort.InvalidateVisual();
-            MyModel.Content = triangle;
+
+            if (EdgesCheckBox != null)
+            {
+                if ((bool)EdgesCheckBox.IsChecked)
+                {
+                    MyModel.Content = triangleEdges;
+                }
+                else
+                {
+                    MyModel.Content = triangle;
+                }
+            }
+            else
+            {
+                MyModel.Content = triangle;
+            }
         }
 
 
@@ -98,6 +125,11 @@ namespace SimplePyramid
                 LengthBox.Text = "10";
                 this.Length = 10;
             }
+            DrawPyramid();
+        }
+
+        private void EdgesCheckBoxChanged(object sender, RoutedEventArgs e)
+        {
             DrawPyramid();
         }
        
